fba6fc94c387fa88916c82a82400321f06e53f3f
Your Name <you@example.com>
2024-07-25 22:57:47 +0100

    need to fix javascript

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const link = document.querySelectorAll("a");
+const profileButton = document.querySelector("#profile");
+const main = document.querySelector("#main");
+const profileId = document.querySelector("#profile-part");
+// const games = document.querySelector("#games");
 
 let j = 0;
 if (j === 0)
@@ -17,3 +21,22 @@ for (let i = 0; i < link.length ;i++)
         link[i].classList.add('link');
     });
 };
+
+function profile(event)
+{
+    event.preventDefault();
+    main.style.display = "none";
+    profileId.style.display = "block";
+    console.log("test profile..");
+}
+
+profileButton.addEventListener("click", profile);
+
+function mainFunction()
+{
+    // console.log("test mic..");
+    main.style.display = "block";
+    profileId.style.display = "none";
+}
+
+profileButton.addEventListener("click", mainFunction);
