b6fdbc691613414f00799448d9f710c74e51ef3a
Your Name <you@example.com>
2024-08-10 18:37:50 +0100

    some improvment using default bootstrap buttons

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 // let defaultName = username || 'Stranger';
 // console.log(defaultName); // Prints: Stranger
 
-const links = document.querySelectorAll("a");
+const links = document.querySelectorAll("button");
 
 links[0].classList.add('link');
 
 links.forEach ((link)=> {
     link.addEventListener("click", (event)=> {
         links.forEach (link => link.classList.remove('link'));
-        event.preventDefault();
+        // event.preventDefault();
         link.classList.add('link');
     })
 });
