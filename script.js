2d12cfeedf974be80132970128bee9340bfcc007
mhassani <mhassani@My-Home.myguest.virtualbox.org>
2024-08-20 21:57:38 +0100

    users with different style when selected in chat

--- script.js
+++ script.js
@@ -29,13 +29,25 @@ const data_example = async() => {
 const data_characters = async() => {
     const characters = await data_example();
     characters.forEach(character => {
-        const users = document.createElement("div");
-        document.querySelector("#chats").appendChild(users);
-        users.innerHTML = character.name;
-        // document.querySelector("#chat-pic").style.backgroundImage  = `url("${character.images[0]}")` change when click on the user only.
-        document.querySelector("#secondd h3").innerHTML = character.name;
+        const user = document.createElement("div");
+        document.querySelector("#chats").appendChild(user);
+        user.innerHTML = character.name;
+        // document.querySelector("#chats div").style.border = "none";
+        user.addEventListener("click", ()=> {
+            const users = document.querySelectorAll("#chats div");
+            users.forEach(userr => {
+                userr.style.width = "90%";
+                userr.style.boxShadow = "0 0 5px #0e2c2e";
+            })
+            user.style.width = "98%";
+            user.style.boxShadow = "0 0 5px #9bf9ff";
+            document.querySelector("#chat-pic").style.backgroundImage  = `url("${character.images[0]}")`;
+            document.querySelector("#secondd h3").innerHTML = character.name;
+        })
     })
 }
+
+//  change when click on the user only.
 
 data_characters();
 
