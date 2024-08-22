e89709e329cf7cb5d81651bf8d5c70b6df609315
mhassani <mhassani@My-Home.myguest.virtualbox.org>
2024-08-22 16:37:29 +0100

    login and home manipulation using js

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ links.forEach ((link)=> {
     })
 });
 
+const loginBtn = document.querySelector(".login-btn");
+const loginPart = document.querySelector("#login-parent");
+const navBar = document.querySelector("#nav");
+const mainPart = document.querySelector("#main");
+const profilePart = document.querySelector("#profile-part");
+const chatPart = document.querySelector("#chat-part");
+const setting = document.querySelector("#setting-part");
+
+loginBtn.addEventListener("click", ()=> {
+    navBar.style.display = "block";
+    mainPart.style.display = "block";
+    loginPart.style.display = "none";
+})
+
+const logoutBtn = document.querySelector("#logout");
+
+logoutBtn.addEventListener("click", ()=> {
+    setting.style.display = "none";
+    chatPart.style.display = "none";
+    profilePart.style.display = "none";
+    navBar.style.display = "none";
+    mainPart.style.display = "none";
+    loginPart.style.display = "flex";
+})
