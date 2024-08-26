e20d16ed03094f776cad7927d8758be2cf16426f
mhassani <mhassani@My-Home.myguest.virtualbox.org>
2024-08-26 22:01:40 +0100

    setting nav done

--- scripts/setting.js
+++ scripts/setting.js
@@ -13,3 +13,36 @@ const settingFunction = () => {
 }
 
 settingButton.addEventListener("click", settingFunction);
+
+// nav manipulation style.
+const settingNavBtn = document.querySelectorAll(".setting-nav-btn");
+settingNavBtn[0].classList.add('styled-nav-btn');
+
+settingNavBtn.forEach ((frdNavBtn)=> {
+    frdNavBtn.addEventListener("click", (event)=> {
+        settingNavBtn.forEach (frdNavBtn => {frdNavBtn.classList.remove('styled-nav-btn')});
+        frdNavBtn.classList.add('styled-nav-btn');
+    })
+});
+
+const profileSetting = document.querySelector("#profile-setting");
+const themes = document.querySelector("#themes");
+const options = document.querySelector("#options");
+
+settingNavBtn[0].addEventListener("click", (event)=> {
+    profileSetting.style.display = "flex";
+    options.style.display = "none";
+    themes.style.display = "none";
+})
+
+settingNavBtn[1].addEventListener("click", ()=> {
+    profileSetting.style.display = "none";
+    options.style.display = "none";
+    themes.style.display = "flex";
+})
+
+settingNavBtn[2].addEventListener("click", ()=> {
+    profileSetting.style.display = "none";
+    options.style.display = "flex";
+    themes.style.display = "none";
+})
